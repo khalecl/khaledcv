becbde0f68845d3271f863cbbc59a77c06b34425
khaled rady <46851194+khalecl@users.noreply.github.com>
2025-10-21 13:51:32 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Cache name
+const CACHE_NAME = 'khutbah-translator-v1';
+
+// Files to cache
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+// Fetch event to serve cached content
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
